07845ff51e235a262e0659b58348f1375d78aa9c
akurnenkov <55654496+KurnenkovArtem@users.noreply.github.com>
2026-02-12 21:41:02 +0300

    Iter 7

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -7,11 +7,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get("SECRET_KEY", default=get_random_secret_key())
 
-DEBUG = os.environ.get("DEBUG", "False")
+DEBUG = os.getenv("DEBUG", "").lower() == "true"
 
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(",")
-
-STATIC_ROOT = "/app/collected_static"
 
 INSTALLED_APPS = [
     "django.contrib.admin",
