d23042cf9a1966b13674044a35305194576ece01
akurnenkov <55654496+KurnenkovArtem@users.noreply.github.com>
2026-02-14 01:01:45 +0300

    Iter 20

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -11,6 +11,8 @@ DEBUG = os.getenv("DEBUG", "").lower() == "true"
 
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(",")
 
+CORS_ORIGIN_ALLOW_ALL = True
+
 INSTALLED_APPS = [
     "django.contrib.admin",
     "django.contrib.auth",
@@ -85,7 +87,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = "ru-RU"
 
 TIME_ZONE = "UTC"
 
